520a9a5128866919ec0674a9d94e8597b793fd14
DGF <dgfnydx@163.com>
2026-04-02 12:56:51 +0800

    feat:seo优化

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,5 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import HomeView from '../views/HomeView.vue';
+import { getToolById } from '../registry/toolRegistry';
+import i18n from '../i18n';
 
 const routes = [
   {
@@ -18,6 +20,18 @@ const routes = [
 const router = createRouter({
   history: createWebHistory(),
   routes
+});
+
+router.afterEach((to) => {
+  const { t } = i18n.global as any;
+  if (to.name === 'tool' && to.params.id) {
+    const tool = getToolById(to.params.id as string);
+    if (tool) {
+      document.title = `${t(`tools.${tool.id}.name`)} - OmniTool`;
+      return;
+    }
+  }
+  document.title = 'OmniTool - All-in-One Online Toolbox';
 });
 
 export default router;
